66e708818c2fa8ca304117e6fa3efd1652d22233
l-ram <larryramjutton@gmail.com>
2025-01-29 01:44:38 +0100

    favourites functionality is complete

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 // import { getFavourites } from "../db/db_apis";
 import ReactGA from "react-ga4";
-import { UseGetFavourites } from "../db/db_apis";
+import { UseGetFavourites, addToBasket } from "../db/db_apis";
 import { useSession } from "../context/SessionContext";
 import { Database } from "../types/db";
 import "../pages/favourites.scss";
+import { supabase } from "../config/supabase.config";
 
 const Favourites = () => {
   const session = useSession();
@@ -13,13 +14,11 @@ const Favourites = () => {
 
   const [searchText, setSearchText] = useState<string>("");
   const [filteredFaves, setFilteredFaves] = useState(faves?.data);
-  console.log(searchText);
-  console.log(filteredFaves);
   const onSearchChange = (search: string) => {
     setSearchText(search);
   };
   useEffect(() => {
-    const newfilteredFavourites = filteredFaves?.filter((faveFilter) => {
+    const newfilteredFavourites = faves?.data?.filter((faveFilter) => {
       return faveFilter.name.toLowerCase().includes(searchText);
     });
 
@@ -30,6 +29,18 @@ const Favourites = () => {
     ReactGA.set({ page: window.location.href + window.location.search });
     console.log("ga ran");
   }, []);
+
+  const handleRemoveFavourite = async (productId: number) => {
+    await supabase.from("favourites").delete().eq("product_id", productId);
+
+    const { data } = await supabase
+      .from("favourites")
+      .select("*")
+      .eq("user_id", session?.user.id as string)
+      .returns<Database["public"]["Tables"]["favourites"]["Row"][]>();
+
+    setFilteredFaves(data!);
+  };
 
   return (
     <div>
@@ -43,8 +54,20 @@ const Favourites = () => {
           (fave: Database["public"]["Tables"]["favourites"]["Row"]) => (
             <div className="grid__card" key={fave.product_id}>
               <p>{fave.name}</p>
-              <button>Remove from favourites</button>
-              <button>Add to cart</button>
+              <button
+                onClick={() => {
+                  handleRemoveFavourite(fave.product_id);
+                }}
+              >
+                Remove from favourites
+              </button>
+              <button
+                onClick={() =>
+                  addToBasket(fave.product_id, session?.user.id as string)
+                }
+              >
+                Add to cart
+              </button>
             </div>
           )
         )}
